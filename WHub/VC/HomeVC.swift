c0ca03c3d0ab817e04c0a8594d2ba57e531f176b
shihao <358968601@qq.com>
2019-07-01 17:43:59 +0800

    update

---
+++ WHub/VC/HomeVC.swift
@@ -0,0 +1,14 @@
+//
+//  HomeVC.swift
+//  WHub
+//
+//  Created by wtoe on 2019/7/1.
+//  Copyright © 2019 shihao. All rights reserved.
+//  列表形式不同功能入口
+
+import Foundation
+
+class HomeVC: BaseVC {
+    
+    
+}
