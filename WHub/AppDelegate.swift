f2ebecabc9d4ae36492994c0f0579566da818520
selfcook <358968601@qq.com>
2020-05-06 22:17:36 +0800

    update

--- WHub/AppDelegate.swift
+++ WHub/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+        //test_head
         //test
         //test
         window = UIWindow(frame: UIScreen.main.bounds)
