e58011e4990960cfbe94964fc811788a6904b137
selfcook <358968601@qq.com>
2020-05-06 21:45:54 +0800

    branch_test

--- WHub/AppDelegate.swift
+++ WHub/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = BaseTabbarController()
         window?.makeKeyAndVisible()
-        
+        //branch_test
         return true
     }
 
