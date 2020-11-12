52c649347d4672dbfe7deeda3a7ac2e7c68a5157
shihao <358968601@qq.com>
2020-11-12 22:24:36 +0800

    11111

--- WHub/AppDelegate.swift
+++ WHub/AppDelegate.swift
@@ -12,6 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    //master
+    //test1
     //test1 vscode
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
